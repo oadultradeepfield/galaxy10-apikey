250bbed2a5b6e0a333438b1db5bf0b7cf08048ac
Phanuphat Srisukhawasu <phanuphat.srisukhawasu@gmail.com>
2025-01-13 09:16:03 +0800

    chore(frontend): update font styling

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,11 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "@mui/material";
+import theme from "./theme.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </StrictMode>
 );
